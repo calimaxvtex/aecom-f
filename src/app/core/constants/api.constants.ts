2442109c309cea8bbd1733d1a4940f0959e8d067
Luis Garcia <luiscarlos.garcia@calimax.com.mx>
2025-09-19 17:50:31 -0700

    change const

--- src/app/core/constants/api.constants.ts
+++ src/app/core/constants/api.constants.ts
@@ -4,7 +4,6 @@ export const API_CONFIG = {
     // BASE_URL: 'http://10.10.254.127:3012',
     BASE_URL: 'https://ec.calimax.digital',
     BASE_URL_IMG: 'http://10.10.254.127:3013',
-    BASE_URL: 'http://localhost:3012',
     // BASE_URL: 'http://10.10.250.168:3012',
     ENDPOINTS: {
         MENU: { CRUD: '/api/menu/v1' },
@@ -15,14 +14,7 @@ export const API_CONFIG = {
         SUC: { CRUD: '/api/admsuc/v1' },
         BANNER: { UPLOAD: '/upload_banner' },
         SUCURSAL: { CRUD: '/api/admsucursal/v1' },
-        PROYECTO: { CRUD: '/api/admproy/v1' }
-        MENU: { CRUD: '/api/menu/v1' },
-        SPCONFIG: { GET: '/api/spconfig/v1' },
-        APIC: { CONFIG: '/apic/config' },
-        COLL: { CRUD: '/api/admcoll/v1' },
-        COLLD: { CRUD: '/api/admcolld/v1' },
-        SUC: { CRUD: '/api/admsuc/v1' },
-        BANNER: { UPLOAD: '/upload_banner' },
+        PROYECTO: { CRUD: '/api/admproy/v1' },
         RECETA: { CRUD: '/api/admrcta/v1' }
     }
 };
@@ -37,8 +29,7 @@ export const API_URLS = {
     SUC_CRUD: `${API_CONFIG.BASE_URL}${API_CONFIG.ENDPOINTS.SUC.CRUD}`,
     BANNER_UPLOAD: `${API_CONFIG.BASE_URL_IMG}${API_CONFIG.ENDPOINTS.BANNER.UPLOAD}`,  // ✅ Usa BASE_URL_IMG
     SUCURSAL_CRUD: `${API_CONFIG.BASE_URL}${API_CONFIG.ENDPOINTS.SUCURSAL.CRUD}`,
-    PROYECTO_CRUD: `${API_CONFIG.BASE_URL}${API_CONFIG.ENDPOINTS.PROYECTO.CRUD}`
-    BANNER_UPLOAD: `${API_CONFIG.BASE_URL_IMG}${API_CONFIG.ENDPOINTS.BANNER.UPLOAD}`,  // ✅ Usa BASE_URL_IMG
+    PROYECTO_CRUD: `${API_CONFIG.BASE_URL}${API_CONFIG.ENDPOINTS.PROYECTO.CRUD}`,
     RECETA_CRUD: `${API_CONFIG.BASE_URL}${API_CONFIG.ENDPOINTS.RECETA.CRUD}`
 };
 
